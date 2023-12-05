93c13d77f87856334bf6f2ed1ddc5de63e6ec6f6
Guilherme-Joviniano <guilherme.sousa@pagtel.com.br>
2023-12-05 15:01:40 -0300

    chore: add multiples ways to extract query and path variables in methods

--- firstproject/src/main/java/br/com/joviniano/controllers/GreetingController.java
+++ firstproject/src/main/java/br/com/joviniano/controllers/GreetingController.java
@@ -1,16 +1,33 @@
 package br.com.joviniano.controllers;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/greetings")
 public class GreetingController {
 
+  @GetMapping("/{name}")
+  public String greeting(
+      @PathVariable String name) {
+    return "Hello " + name;
+  }
+
   @GetMapping("")
-  public String greeting() {
-    return "Hello World!";
+  public String greeting2(
+      @RequestParam String id,
+      @RequestParam String name) {
+    return "Hello " + id + " " + name;
+  }
+
+  @GetMapping("/query")
+  public String greeting3(@RequestParam Map<String, String> allParams) {
+    return "O parametro com é " + allParams.entrySet();
   }
 
 }
