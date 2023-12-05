a1af0c657ee028a6ef088c7b9eb684fe91b54e4c
Guilherme-Joviniano <guilherme.sousa@pagtel.com.br>
2023-12-05 15:24:59 -0300

    chore: first look at project

---
+++ job_offer_manager/src/main/java/br/com/joviniano/job_offer_manager/JobOfferManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.joviniano.job_offer_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobOfferManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobOfferManagerApplication.class, args);
+	}
+
+}
---
+++ job_offer_manager/src/test/java/br/com/joviniano/job_offer_manager/JobOfferManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joviniano.job_offer_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobOfferManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
