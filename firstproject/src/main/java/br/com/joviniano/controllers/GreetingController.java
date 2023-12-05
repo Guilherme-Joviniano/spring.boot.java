98f927a4d869e226f0ff016453c2e0605ec3b35d
Guilherme-Joviniano <guilherme.sousa@pagtel.com.br>
2023-12-05 14:56:01 -0300

    refactor: moving package to new directory and add ComponentScan into bootstrap application

--- firstproject/src/main/java/br/com/joviniano/controllers/GreetingController.java
+++ firstproject/src/main/java/br/com/joviniano/controllers/GreetingController.java
@@ -1,4 +1,4 @@
-package br.com.joviniano.firstproject.controllers;
+package br.com.joviniano.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
--- firstproject/src/main/java/br/com/joviniano/firstproject/FirstProjectApplication.java
+++ firstproject/src/main/java/br/com/joviniano/firstproject/FirstProjectApplication.java
@@ -2,8 +2,10 @@ package br.com.joviniano.firstproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.joviniano")
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +13,3 @@ public class FirstProjectApplication {
 	}
 
 }
-
